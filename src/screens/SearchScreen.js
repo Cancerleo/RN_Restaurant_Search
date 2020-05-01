f6f84de19e624b5416491419ce4ff4e9690fc419
Cancerleo <sandra.iriandini@gmail.com>
2020-05-01 18:59:10 +0200

    Modify Search screen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,34 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
-import yelp from "../api/yelp";
+import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const searchApi = async (searchTerm) => {
-    console.log("Hi There!");
-
-    try {
-      const response = await yelp.get("/search", {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: "Amsterdam",
-        },
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      setErrorMessage("Something went wrong");
-    }
-  };
-
-  // call searchAPI
-  useEffect(() => {
-    searchApi("pasta");
-  }, []);
+  const [searchApi, results, errorMessage] = useResults();
 
   return (
     <View>
@@ -39,6 +17,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text> We have found {results.length} results</Text>
+      <ResultsList title="Cost Effective" />
+      <ResultsList title="Big Pricier" />
+      <ResultsList title="Big Spender" />
     </View>
   );
 };
