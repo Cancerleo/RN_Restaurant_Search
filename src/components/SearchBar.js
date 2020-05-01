8aa1bd582681d48a5c0732cad39616672733c66b
Cancerleo <sandra.iriandini@gmail.com>
2020-05-01 14:51:23 +0200

    Implement useState in the searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,17 +2,26 @@ import React from "react";
 import { View, TextInput, StyleSheet } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
   return (
     <View style={styles.backgroundStyle}>
-      <Feather name="search" size={30} />
-      <TextInput style={styles.inputStyle} placeholder="Search" />
+      <Feather name="search" style={styles.iconStyle} />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.inputStyle}
+        placeholder="Search"
+        value={term}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   backgroundStyle: {
+    marginTop: 10,
     backgroundColor: "#f0eeee",
     height: 50,
     borderRadius: 5,
@@ -20,9 +29,13 @@ const styles = StyleSheet.create({
     flexDirection: "row",
   },
   inputStyle: {
-    borderColor: "black",
-    borderWidth: 1,
     flex: 1,
+    fontSize: 18,
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
+    marginHorizontal: 15,
   },
 });
 
